a29001982779626ed72dc48cdfe02f9d4ae87422
wassim <wassimrached404@gmail.com>
2024-10-11 16:37:24 +0100

    fix backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  backendUrl: 'meals-master.azurewebsites.net',
+  backendUrl: 'https://meals-master.azurewebsites.net',
 };
