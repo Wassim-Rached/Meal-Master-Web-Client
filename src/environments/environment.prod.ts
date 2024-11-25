82f564828c0456c87d24b59b30d0a6b41cf32f55
wassim <wassimrached404@gmail.com>
2024-11-25 20:46:10 +0100

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  backendUrl: 'https://meals-master.azurewebsites.net',
+  backendUrl: 'https://meal-master.azurewebsites.net',
   messageDisplayDurationInMs: 10000,
 };
